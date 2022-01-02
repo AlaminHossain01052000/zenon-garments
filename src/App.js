60b3ef8703ae23d8dac28972d4118265f14461d6
Yasin440 <eiasin440@gmail.com>
2022-01-02 15:19:31 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header/Header'
 import ContactUs from './Pages/Contact/ContactUs/ContactUs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <ContactUs></ContactUs>
-    </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/contact_us' element={<ContactUs />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
   );
 }
 
--- src/Header/header.js
+++ src/Header/header.js
@@ -2,55 +2,56 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import DehazeIcon from '@mui/icons-material/Dehaze';
-import useAuth from '../../../Hooks/useAuth';
+import logo from '../Media/img/logo.png'
+// import useAuth from '../../../Hooks/useAuth';
 
 const Header = () => {
-    const { user, logOut } = useAuth();
+    // const { user, logOut } = useAuth();
     return (
         <div className="header">
             <nav className="navbar navbar-expand-lg">
                 <div className="container">
-                    <span className="navbar-brand mb-0 h1">NEWS</span>
+                    <span className="navbar-brand mb-0 h1"><img width="180px" src={`${logo}`} alt="logo" /></span>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <DehazeIcon />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink to='/home'>Home</NavLink>
+                                <NavLink to='/home'>HOME</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to='/news/Covid-19'>Covid-19</NavLink>
+                                <NavLink to='/news/Covid-19'>ABOUT US</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to='/news/Entertainment'>Entertainment</NavLink>
+                                <NavLink to='/news/Entertainment'>SERVICES</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to='/news/Travel'>Travel</NavLink>
+                                <NavLink to='/news/Travel'>PROJECTS</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to='/news/Business'>Business</NavLink>
+                                <NavLink to='/news/Business'>PAGE</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to='/news/Sports'>Sports</NavLink>
+                                <NavLink to='/news/Sports'>BLOG</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to='/news/Others'>Others</NavLink>
+                                <NavLink to='/news/Others'>CONTACT</NavLink>
                             </li>
-                            {user.email &&
-                                <li className="nav-item">
-                                    <NavLink to='/profile'>Profile</NavLink>
-                                </li>
-                            }
+                            {/* {user.email && */}
+                            <li className="nav-item">
+                                <NavLink to='/profile'>ADMIN</NavLink>
+                            </li>
+                            {/* } */}
                         </ul>
                         <form className="d-flex">
-                            <span className="userName">{user?.displayName}</span>
+                            {/* <span className="userName">{user?.displayName}</span>
                             {
                                 user.email ?
                                     <NavLink to='/signIn' onClick={logOut}>SignOut</NavLink>
-                                    :
-                                    <NavLink to='/signIn'>SignIn</NavLink>
-                            }
+                                    : */}
+                            <NavLink to='/signIn'>SIGN IN</NavLink>
+                            {/* } */}
                         </form>
                     </div>
                 </div>
