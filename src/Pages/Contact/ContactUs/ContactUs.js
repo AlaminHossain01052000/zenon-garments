22473f316512c52338f942c5b9eed145a24d120d
Al Amin <alaminhossain2000and@gmail.com>
2022-01-03 13:12:38 +0600

    contact change

--- src/Pages/Contact/ContactUs/ContactUs.js
+++ src/Pages/Contact/ContactUs/ContactUs.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ContactFooter from '../ContactFooter/ContactFooter';
 import ContactUsBanner from '../ContactUsBanner/ContactUsBanner';
 import ContactUsBlog from '../ContactUsBlog/ContactUsBlog';
-
+import './ContactUs.css';
 const ContactUs = () => {
     return (
         <div>
+            <h1>Hello</h1>
             <ContactUsBanner></ContactUsBanner>
             <ContactUsBlog></ContactUsBlog>
             <ContactFooter></ContactFooter>
