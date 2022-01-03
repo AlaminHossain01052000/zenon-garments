eefec9029b2994037f274f1060d9ffe14c4d394d
Yasin440 <eiasin440@gmail.com>
2022-01-03 12:11:38 +0600

    our blog-done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import OurBlog from './OurBlog/OurBlog';
 import OurServices from './OurServices/OurServices';
 
 const Home = () => {
     return (
         <div>
             <OurServices></OurServices>
+            <OurBlog></OurBlog>
         </div>
     );
 };
