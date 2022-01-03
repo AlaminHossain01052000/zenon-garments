0ef5010fb059642892a199aa5efaf2b58e10601a
Yasin440 <eiasin440@gmail.com>
2022-01-03 12:28:31 +0600

    blog+services

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Header from '../../Header/header';
 import Footer from "../../Footer/Footer";
+import OurServices from '../OurServices/OurServices';
+import OurBlog from '../OurBlog/OurBlog';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-
+            <OurServices></OurServices>
+            <OurBlog></OurBlog>
             <Footer></Footer>
         </div>
     );
